06258dd8f19061aefa2bc7a1201f606569fbef57
romanell0 <diesel155@mail.ru>
2015-09-16 10:09:29 +1100

    Страница паспорт, страницы новостей

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+    console.log('123');
+    $(".fancybox").fancybox();
+        $("#owl-demo").owlCarousel({
+
+            navigation: true, // Show next and prev buttons
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            singleItem: true
+
+            // "singleItem:true" is a shortcut for:
+            // items : 1, 
+            // itemsDesktop : false,
+            // itemsDesktopSmall : false,
+            // itemsTablet: false,
+            // itemsMobile : false
+
+        });
+});
