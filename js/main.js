eabad5cddf6d2c24f0ca854a43acc9a253a7bce1
romanell0 <diesel155@mail.ru>
2015-09-23 10:42:29 +1100

    Кнопки взад-назад, для слайдера, меню для моб.устройств

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 $(function () {
-    console.log(';-D');
+    var owl = $("#owl-slider");
     $(".fancybox").fancybox();
-        $("#owl-demo").owlCarousel({
+    owl.owlCarousel({
 
-            navigation: true, // Show next and prev buttons
-            slideSpeed: 300,
-            paginationSpeed: 400,
-            singleItem: true,
-            navigationText: ['', '']
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        navigationText: ['', '']
 
-            // "singleItem:true" is a shortcut for:
-            // items : 1, 
-            // itemsDesktop : false,
-            // itemsDesktopSmall : false,
-            // itemsTablet: false,
-            // itemsMobile : false
+        // "singleItem:true" is a shortcut for:
+        // items : 1, 
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
 
-        });
+    });
+    $(".next").click(function () {
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function () {
+        owl.trigger('owl.prev');
+    })
 });
